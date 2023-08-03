db2b40b8602fbe32d8a38ed43bb1a10a289f081d
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-08-02 23:12:00 -0300

    Feat: Criação base com next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-lime-500">
+      <h1 className="text-neutral-900">Hello</h1>
+    </main>
+  )
+}
