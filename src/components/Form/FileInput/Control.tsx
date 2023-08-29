39fbc4068b6dc11141b81a940e1a085fe6c5d5ea
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-08-28 21:45:52 -0300

    Feat: Context e Preview da Imagem

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,7 +1,30 @@
-import { ComponentProps } from 'react'
+'use client'
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
 export function Control(props: ControlProps) {
-  return <input type="file" className="sr-only" id="photo" {...props} />
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files
+
+    if (!files) {
+      return null
+    }
+
+    const fileList = Array.from(files)
+    onFilesSelected(fileList)
+  }
+
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      id={id}
+      onChange={handleFilesSelected}
+      {...props}
+    />
+  )
 }
--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,35 @@
+'use client'
 import { User } from 'lucide-react'
+import { useFileInput } from './Root'
+import { useMemo } from 'react'
+import Image from 'next/image'
 
 export function ImagePreview() {
-  return (
-    <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
-      <User className="h-8 w-8  text-violet-500" />
-    </div>
-  )
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+    if (files.length === 0) {
+      return null
+    }
+
+    return URL.createObjectURL(files[0])
+  }, [files])
+
+  if (previewURL === null) {
+    return (
+      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
+        <User className="h-8 w-8  text-violet-500" />
+      </div>
+    )
+  } else {
+    return (
+      <Image
+        src={previewURL}
+        alt=""
+        className="h-16 w-16 rounded-full object-cover"
+        height={100}
+        width={100}
+      />
+    )
+  }
 }
