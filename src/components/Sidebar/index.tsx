eb14ffee45ec410847ab880868792f579d73b15a
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-09-04 14:55:40 -0300

    Feat: Responsividade da Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ import * as Input from '../Form/Input'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white px-5 py-6 lg:relative lg:right-auto lg:w-80 lg:border-r lg:py-8">
       <Logo />
 
       <Input.Root>
